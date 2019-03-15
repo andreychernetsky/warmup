1d15099d970a24da190c16a21cfa45a3206eb4f2
Андрей черенецкий <you@example.com>
2019-03-15 12:23:47 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+// (0 °C × 9/5) + 32 = 32 °F
+
 module.exports = function warmup(temperature) {
-  // your implementation here
+  return Math.round(temprature * (9/5)+32)
 };
+
+
+/*
+function warmup(temperature) {
+  return Math.round(-20 * (9/5)+32)
+};
+let temp = -20;
+let temp = 10;
+console.log(warmup(-20));//-4
+console.log(warmup(10));//50*/
+
